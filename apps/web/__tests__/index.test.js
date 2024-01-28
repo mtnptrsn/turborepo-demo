87fd6c9762699ca0829c15af3572eabeab42d9b1
Mårten Pettersson <mtnptrsn@gmail.com>
2024-01-28 09:24:36 +0100

    add integration tests with rtl

---
+++ apps/web/__tests__/index.test.js
@@ -0,0 +1,22 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Home from "../src/app/page";
+
+describe("Home", () => {
+  it("renders a heading and a button", () => {
+    window.alert = jest.fn();
+
+    render(<Home />);
+    // check if there's a heading
+    const heading = screen.getByRole("heading", { level: 1 });
+    expect(heading).toBeInTheDocument();
+
+    // check if there's a button with label "Click Me"
+    const button = screen.getByRole("button", { name: "Click Me" });
+    expect(button).toBeInTheDocument();
+
+    // clicks the button and confirms an alert shows up
+    button.click();
+    expect(window.alert).toHaveBeenCalledWith("Button was clicked");
+  });
+});
