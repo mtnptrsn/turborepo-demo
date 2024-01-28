9143e7c3ad23acfe2d929d0f8c5f7e13e7f55d74
Mårten Pettersson <mtnptrsn@gmail.com>
2024-01-28 12:20:35 +0100

    update

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,9 +5,8 @@ import { Button } from "@repo/ui/button";
 export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen gap-2">
-      <h1 className="text-2xl">Main App</h1>
+      <h1 className="text-2xl">Main App!</h1>
       <Button onClick={() => alert("Button was clicked")} label="Click Me" />
-      <div>A div instead with more content</div>
     </main>
   );
 }
