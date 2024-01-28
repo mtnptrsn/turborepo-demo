89b25a39c6e1e52a0a3fdefa5d3a69b36f0d106d
Mårten Pettersson <mtnptrsn@gmail.com>
2024-01-28 10:47:19 +0100

    add new p

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Page(): JSX.Element {
     <main className="flex flex-col items-center justify-center min-h-screen gap-2">
       <h1 className="text-2xl">Main App</h1>
       <Button onClick={() => alert("Button was clicked")} label="Click Me" />
+      <p>A new paragraph</p>
     </main>
   );
 }
