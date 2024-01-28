648c31b89b455bb841b3d3c1be836ad4b7546508
Mårten Pettersson <mtnptrsn@gmail.com>
2024-01-28 10:06:56 +0100

    fix lint issues

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -6,10 +6,7 @@ export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen gap-2">
       <h1 className="text-2xl">Main App</h1>
-      <Button
-        onClick={() => alert("Button was clicked")}
-        label="Click Me"
-      ></Button>
+      <Button onClick={() => alert("Button was clicked")} label="Click Me" />
     </main>
   );
 }
--- packages/config-eslint/next.js
+++ packages/config-eslint/next.js
@@ -41,5 +41,7 @@ module.exports = {
   // add rules configurations here
   rules: {
     "import/no-default-export": "off",
+    "no-alert": "off",
+    "@typescript-eslint/no-confusing-void-expression": "off",
   },
 };
