24539d99b4fdc95023136788906182296d570603
Mårten Pettersson <mtnptrsn@gmail.com>
2024-01-28 13:45:22 +0100

    add fake pipeline to fix dependency issue

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Page(): JSX.Element {
     <main className="flex flex-col items-center justify-center min-h-screen gap-2">
       <h1 className="text-2xl">This is the title</h1>
       <h2 className="text-2xl">This is the subtitle</h2>
+      <h2 className="text-2xl">This is the subtitle</h2>
       <Button onClick={() => alert("Button was clicked")} label="Click Me" />
     </main>
   );
