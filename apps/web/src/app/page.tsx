c8b4034a95e0bced0db565b50f48c7494d971d0d
Mårten Pettersson <mtnptrsn@gmail.com>
2024-01-28 11:11:11 +0100

    update

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Page(): JSX.Element {
     <main className="flex flex-col items-center justify-center min-h-screen gap-2">
       <h1 className="text-2xl">Main App</h1>
       <Button onClick={() => alert("Button was clicked")} label="Click Me" />
-      <p>A new paragraph with even more text and more</p>
+      <p>A new paragraph with even more text and more and MORE</p>
     </main>
   );
 }
